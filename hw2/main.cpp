01d184437a27b19acd5381938c6c6967f4fd7ed7
CTHua <59926496+CTHua@users.noreply.github.com>
2020-11-19 16:46:53 +0800

    Create main.cpp

---
+++ hw2/main.cpp
@@ -0,0 +1,312 @@
+//==============================================================//
+//|         NCCU CS Data Structure Homework 2 by.CTHua         |//
+//==============================================================//
+#include <bits/stdc++.h>
+#include "skiplist.h"
+#define pb push_back
+#define PII pair<int, int>
+#define mkp make_pair
+#define Lint long long
+#define N_max 28
+#define R_max 1073741824 //2^30
+using namespace std;
+//functoin for Sorted-Array
+int insertSorted(int arr[], int n, int key, int capacity)
+{
+    // Cannot insert more elements if n is already
+    // more than or equal to capcity
+    if (n >= capacity)
+        return n;
+
+    int i;
+    for (i = n - 1; (i >= 0 && arr[i] > key); i--)
+        arr[i + 1] = arr[i];
+    arr[i + 1] = key;
+    return (n + 1);
+}
+int binarySearch(int arr[], int low, int high, int key)
+{
+    if (high < low)
+        return -1;
+    int mid = (low + high) / 2; /*low + (high - low)/2;*/
+    if (key == arr[mid])
+        return mid;
+    if (key > arr[mid])
+        return binarySearch(arr, (mid + 1), high, key);
+    return binarySearch(arr, low, (mid - 1), key);
+}
+//
+
+//Main
+int main()
+{
+    string command;
+    srand(time(NULL));
+    fstream outfile;
+    while (cin >> command)
+    {
+        if (command == "mset")
+        {
+            multiset<int> SC;
+            outfile.open("./mset.csv", ios::out);
+            outfile << "insert,search" << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./mset.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    double start, end;
+                    outfile.open("./mset.csv", ios::app);
+                    SC.clear();
+                    //
+                    //insert part
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        SC.insert(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        SC.find(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                }
+                //
+                cout << endl;
+            }
+        }
+        else if (command == "set")
+        {
+            set<int> SC;
+            outfile.open("./set.csv", ios::out);
+            outfile << "insert,search" << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./set.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    double start, end;
+                    outfile.open("./set.csv", ios::app);
+                    SC.clear();
+                    //
+                    //insert part
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        SC.insert(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        SC.find(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                }
+                //
+                cout << endl;
+            }
+        }
+        else if (command == "mmap")
+        {
+            multimap<int, int> SC;
+            outfile.open("./mmap.csv", ios::out);
+            outfile << "insert,search" << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./mmap.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    double start, end;
+                    outfile.open("./mmap.csv", ios::app);
+                    SC.clear();
+                    //
+                    //insert part
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        SC.insert(mkp(rand() % R_max + 1, 0));
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        SC.find(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                }
+                //
+                cout << endl;
+            }
+        }
+        else if (command == "map")
+        {
+            map<int, int> SC;
+            outfile.open("./map.csv", ios::out);
+            outfile << "insert,search" << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./map.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    double start, end;
+                    outfile.open("./map.csv", ios::app);
+                    SC.clear();
+                    //
+                    //insert part
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        SC.insert(mkp(rand() % R_max + 1, 0));
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        SC.find(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                }
+                //
+                cout << endl;
+            }
+        }
+        else if (command == "sorted-arr")
+        {
+            vector<int> SC; //sorted_container-3
+            outfile.open("./sarr.csv", ios::out);
+            outfile << "insert,search" << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./sarr.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    double start, end;
+                    outfile.open("./sarr.csv", ios::app);
+                    SC.clear();
+                    //
+                    //insert part
+                    int n = 0;
+                    int *arr = new int[size];
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        n = insertSorted(arr, n, rand() % R_max + 1, size);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        binarySearch(arr, 0, n - 1, (rand() % R_max + 1));
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                    delete arr;
+                }
+                //
+                cout << endl;
+            }
+        }
+        else if (command == "skiplist")
+        {
+            double SKIP_LIST_LVL_RATE;
+            cin >> SKIP_LIST_LVL_RATE;
+            outfile.open("./skip.csv", ios::app);
+            outfile << "insert,search,rate=," << SKIP_LIST_LVL_RATE << endl;
+            outfile.close();
+            for (int N = 10; N <= N_max; N++)
+            {
+                //open file and write N(power of 2)
+                outfile.open("./skip.csv", ios::app);
+                outfile << N << endl;
+                outfile.close();
+                //
+                cout << N << ": "; //output to check run situation
+                int size = 1;
+                size <<= N; //size=2^N
+                for (int t = 1; t <= 10; t++)
+                {
+                    SkipList SC(N, SKIP_LIST_LVL_RATE);
+                    double start, end;
+                    outfile.open("./skip.csv", ios::app);
+                    //
+                    //insert part
+                    start = clock();
+                    for (int i = 0; i < size; i++)
+                        SC.insert(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << ",";
+                    //
+                    //search part
+                    start = clock();
+                    for (int i = 0; i < 1e5; i++)
+                        SC.find(rand() % R_max + 1);
+                    end = clock();
+                    outfile << (end - start) / CLOCKS_PER_SEC << endl;
+                    //
+                    outfile.close();
+                    cout << t << " "; //output to check run situation
+                }
+                //
+                cout << endl;
+            }
+        }
+    }
+    return 0;
+}
